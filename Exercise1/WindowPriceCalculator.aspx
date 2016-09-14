19145bffe3ac46bd6e6bf2a84a79a5edf34f2552
Vesa Kivistö <kivisto.vesku@gmail.com>
2016-09-15 01:01:43 +0300

    Exercise 1 done

---
+++ Exercise1/WindowPriceCalculator.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="WindowPriceCalculator.aspx.cs" Inherits="WindowPriceCalculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div id="content">
+        <asp:Label ID="lblWindowWidth" runat="server" Text="Window width (mm): "></asp:Label>
+        <asp:TextBox ID="txtWindowWidth" runat="server"></asp:TextBox><br />
+        <asp:Label ID="lblWindowHeight" runat="server" Text="Window height (mm): "></asp:Label>
+        <asp:TextBox ID="txtWindowHeight" runat="server"></asp:TextBox><br />
+        <asp:Label ID="lblFrameWidth" runat="server" Text="Frame width (mm): "></asp:Label>
+        <asp:TextBox ID="txtFrameWidth" runat="server"></asp:TextBox><br />
+        <asp:Button ID="btnCalculate" runat="server" Text="Calculate cost" OnClick="btnCalculate_Click" /><br />
+        <asp:Label ID="lblArea" runat="server" Text="Window area: "></asp:Label>
+        <asp:TextBox ID="txtArea" runat="server" Enabled="false"></asp:TextBox><br />
+        <asp:Label ID="lblPerimeter" runat="server" Text="Border perimeter: "></asp:Label>
+        <asp:TextBox ID="txtPerimeter" runat="server" Enabled="false"></asp:TextBox><br />
+        <asp:Label ID="lblCost" runat="server" Text="Cost (without VAT): "></asp:Label>
+        <asp:TextBox ID="txtCost" runat="server" Enabled="false"></asp:TextBox><br />
+    </div>
+    <div id="error">
+        <asp:Label ID="lblError" runat="server"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Exercise1/WindowPriceCalculator.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class WindowPriceCalculator : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnCalculate_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            lblError.Text = "";
+            //Convert cm to meters
+            double windowWidth = double.Parse(txtWindowWidth.Text) / 100;
+            double windowHeight = double.Parse(txtWindowHeight.Text) / 100;
+            double frameWidth = double.Parse(txtFrameWidth.Text) / 100;
+            txtArea.Text = CalculateWindowArea(windowWidth, windowHeight).ToString();
+            txtPerimeter.Text = CalculatePerimeter(windowWidth, windowHeight).ToString();
+            txtCost.Text = CalculateCosts(windowWidth, windowHeight, frameWidth).ToString();
+        }
+        catch (Exception)
+        {
+            lblError.Text = "Check your fields.";
+            txtArea.Text = "";
+            txtPerimeter.Text = "";
+            txtCost.Text = "";
+        }
+    }
+
+    private double CalculateWindowArea(double width, double height)
+    {
+        double area = width * height;
+        return area;
+    }
+
+    private double CalculatePerimeter(double width, double height)
+    {
+        double perimeter = 2 * width + 2 * height;
+        return perimeter;
+    }
+
+    private double CalculateCosts(double width, double height, double frame)
+    {
+        double salesMargin = double.Parse(ConfigurationManager.AppSettings["salesMargin"]);
+        double squarePrice = double.Parse(ConfigurationManager.AppSettings["squarePrice"]);
+        double framePrice = double.Parse(ConfigurationManager.AppSettings["framePrice"]);
+        double workCost = double.Parse(ConfigurationManager.AppSettings["workCost"]);
+        double windowCost = CalculateWindowArea(width, height) * squarePrice;
+        double frameCost = CalculatePerimeter(width, height) * framePrice;
+        double costs = (1 + salesMargin / 100) * (windowCost + frameCost + workCost);
+        return costs;
+    }    
+}
