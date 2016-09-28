b0fcca44e0ab18eb86580f15012510aac46dd271
Vesa Kivistö <kivisto.vesku@gmail.com>
2016-09-29 00:07:52 +0300

    Exercise 3 done

---
+++ Exercise3/Calendar.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Calendar.aspx.cs" Inherits="Calendar" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>Today is: <asp:Label ID="lblCurrentDate" runat="server" /></h1>
+        Selected date: <asp:Label ID="lblSelectedDate" runat="server" /><br />
+        Difference between current date and selected date: <asp:Label ID="lblDifference" runat="server" /><br />
+        <asp:Button ID="btnPreviousYear" runat="server" Text="< Year" OnClick="btnPreviousYear_Click" />
+        <asp:Button ID="btnNextYear" runat="server" Text="Year >   " OnClick="btnNextYear_Click" />
+        <asp:Calendar ID="calDate" runat="server" OnSelectionChanged="calDate_SelectionChanged" AutoPostBack="true" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Exercise3/Calendar.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Calendar : System.Web.UI.Page
+{
+    DateTime currentDate;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        currentDate = DateTime.Today;
+        lblCurrentDate.Text = currentDate.ToString("dd.MM.yyyy");
+        if (!IsPostBack)
+        {
+            calDate.VisibleDate = currentDate;
+            lblSelectedDate.Text = calDate.VisibleDate.ToString("dd.MM.yyyy");
+        }
+    }
+
+    protected void btnPreviousYear_Click(object sender, EventArgs e)
+    {
+        calDate.VisibleDate = calDate.VisibleDate.AddYears(-1);
+    }
+
+    protected void btnNextYear_Click(object sender, EventArgs e)
+    {
+        calDate.VisibleDate = calDate.VisibleDate.AddYears(1);
+    }
+
+    protected void calDate_SelectionChanged(object sender, EventArgs e)
+    {
+        lblSelectedDate.Text = calDate.SelectedDate.ToString("dd.MM.yyyy");
+        TimeSpan span = (calDate.SelectedDate > currentDate) ? calDate.SelectedDate.Subtract(currentDate) : currentDate.Subtract(calDate.SelectedDate);
+        DateTime diff = DateTime.MinValue + span;
+        lblDifference.Text = (diff.Year - 1).ToString() + " years, " + (diff.Month - 1).ToString() + " months, " + (diff.Day - 1).ToString() + " days.";
+    }
+}
