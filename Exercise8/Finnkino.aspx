929a9047687b1879811cf3069c530a759192c626
Vesa Kivistö <kivisto.vesku@gmail.com>
2016-10-16 22:09:01 +0300

    Exercise 8 done

---
+++ Exercise8/Finnkino.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Finnkino.aspx.cs" Inherits="Finnkino" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:ListBox ID="lbTheatres" runat="server" AutoPostBack="True" OnSelectedIndexChanged="lbTheatres_SelectedIndexChanged" Height="350"></asp:ListBox><br />
+        <asp:Label ID="lblMessages" runat="server" style="display:inline-block;margin:20px 0;font-size:18px;"/>
+        <div id="movieDiv" runat="server" style="width:1000px;"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Exercise8/Finnkino.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+public partial class Finnkino : System.Web.UI.Page
+{
+    XmlDocument doc = new XmlDocument();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            try
+            {
+                doc.Load("http://www.finnkino.fi/xml/TheatreAreas/");
+                XmlNodeList theatreNodes = doc.SelectNodes("TheatreAreas/TheatreArea");
+                foreach (XmlNode node in theatreNodes)
+                {
+                    lbTheatres.Items.Add(new ListItem(node["Name"].InnerText, node["ID"].InnerText));
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMessages.Text = ex.Message;
+            }
+
+        }
+    }
+
+    protected void lbTheatres_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        lblMessages.Text = "Current movies in " + lbTheatres.SelectedItem.Text + ".";
+        GetMovieImages();
+    }
+
+    protected void GetMovieImages()
+    {
+        string areaID = lbTheatres.SelectedValue;
+        doc.Load("http://www.finnkino.fi/xml/Schedule/?area=" + areaID + "&dt=" + DateTime.Today.ToString("dd.MM.yyyy"));
+        XmlNodeList imageNodes = doc.SelectNodes("/Schedule/Shows/Show/Images/EventLargeImagePortrait");
+        foreach (XmlNode node in imageNodes)
+        {
+            HtmlGenericControl movieDiv = (HtmlGenericControl)FindControl("movieDiv");
+            HtmlGenericControl img = new HtmlGenericControl("img");
+            img.Attributes["src"] = node.InnerText;
+            movieDiv.Controls.Add(img);
+        }
+    }
+}
